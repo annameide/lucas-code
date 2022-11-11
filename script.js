599d1a8f322d9cbc86c2c7dae90ccbbce54dc2bd
annameide <46717848+annameide@users.noreply.github.com>
2022-11-11 12:46:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 //# p5.js - Konditionen
 // Verwende Konditionen, um die Funktion `drawDay` oder `drawNight` die Variabel `isNight` entsprenchend.
 
-
-
 // The line below adds autocompletion for p5.js which is very Helpful
-/// <reference path="./p5.d.ts" />
+// <reference path="./p5.d.ts" />
 
 // HELP: https://replit.com/@vogelino/P5js-Conditionals
 
 // Here are the docs for P5.js: https://p5js.org/reference/
-let isNight = false;
+let isNight = true;
 
 // The setup function is called once at the beginning
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background(100);
 
   // Decide to draw either the day or the night
   // based on the isNight variable
-  drawNight();
+  if (isNight === true){
+    // here the function is called
+    drawNight();
+  } else {
+    drawDay();
+  }
 }
 
-function draw() {
-  
-}
 
-
+// here the function is defined
 function drawDay() {
+  fill(255, 0, 0);
+  ellipse(100, 100, 100);
+  text('is day', 30, 30);
 }
 
 function drawNight() {
+  fill(255);
+  ellipse(100, 100, 100);
+  text('is night', 30, 30);
 }
