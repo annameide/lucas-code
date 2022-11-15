ab0fc0e7128ca3a47a0f6df01f2cb95c851c3b47
annameide <46717848+annameide@users.noreply.github.com>
2022-11-15 15:02:12 +0100

    next step

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-//# p5.js - Konditionen
-// Verwende Konditionen, um die Funktion `drawDay` oder `drawNight` die Variabel `isNight` entsprenchend.
+// Conditionals
+// Verwende Konditionen, um die Funktion `drawDay` oder `drawNight` die Variabel // `isNight` entsprenchend.
 
 // The line below adds autocompletion for p5.js which is very Helpful
 // <reference path="./p5.d.ts" />
 
 // HELP: https://replit.com/@vogelino/P5js-Conditionals
-
 // Here are the docs for P5.js: https://p5js.org/reference/
-let isNight = true;
+
+// declaration and initialisation of the isNight variable. 
+// for this example the variable is changed manually between true and false
+
+let isNight = false;
 
 // The setup function is called once at the beginning
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(100);
+  background(255);
 
   // Decide to draw either the day or the night
   // based on the isNight variable
   // draw function is only needed if dynamic art is drawn
-  if (isNight === true){
+  if (isNight === true) {
     // here the function is called
     drawNight();
   } else {
@@ -25,12 +28,15 @@ function setup() {
   }
 }
 
+// here the function drawDay is defined. it is called in the if else conditional // statement inside the setup function, which means a function can be called in 
+// another function. 
 
-// here the function is defined
 function drawDay() {
-  fill(255, 0, 0);
-  ellipse(100, 100, 100);
-  text('is day', 30, 30);
+  background(0, 0, 150);
+  fill("#ffd966");
+  ellipse(windowWidth/2, 100, 100);
+  text('is day', windowWidth/2 - 30, 30);
+
 }
 
 function drawNight() {
