697f880ba4d007f15f4234adfba162ed2cb5b046
christiang7 <98482045+christiang7@users.noreply.github.com>
2023-01-30 22:18:03 +0100

    Solving Problem of CSS and p5js size

---
+++ own_website/README.md
@@ -0,0 +1,46 @@
+##Own Website
+
+
+
+Hier sind die Code Beispiele fürs Einfügen
+
+https://codepen.io/Obsidian-Age/pen/EWPEQP | p5 Canvas size by css
+
+
+https://stackoverflow.com/questions/42544599/p5-canvas-cannot-be-sized-by-css | javascript - p5 canvas cannot be sized by css - Stack Overflow
+
+
+https://stackoverflow.com/questions/35660240/how-to-put-p5-js-canvas-in-a-html-div | How to put p5.js canvas in a html div - Stack Overflow
+
+
+
+Hier noch weitere Recherche
+
+https://github.com/processing/p5.js/wiki/Positioning-your-canvas | Positioning your canvas · processing/p5.js Wiki · GitHub
+
+
+https://stackoverflow.com/questions/63403700/how-to-get-p5-js-to-create-canvas-at-certain-position | javascript - How to get p5.js to create canvas at certain position - Stack Overflow
+
+
+https://discourse.processing.org/t/place-a-p5-js-canvas-inside-a-div/27913/6 | Place a p5.js canvas inside a div - p5.js / Beginners - Processing Foundation
+
+
+https://p5js.org/reference/#/p5.Element | reference | p5.Element
+
+
+https://p5js.org/reference/#/p5.Element/parent | reference | parent()
+
+
+https://github.com/processing/p5.js/wiki/Positioning-your-canvas | Positioning your canvas · processing/p5.js Wiki · GitHub
+
+
+https://p5js.org/reference/#/p5.Element/parent | reference | parent()
+
+
+https://p5js.org/get-started/#sketch | get started | p5.js
+
+
+https://p5js.org/reference/#/p5/createCanvas | reference | createCanvas()
+
+
+https://www.ecosia.org/search?q=p5%20canvas%20size%20controlled%20by%20css&addon=firefox&addonversion=4.1.0&method=topbar | p5 canvas size controlled by css - Ecosia - Web
--- own_website/script.js
+++ own_website/script.js
@@ -3,15 +3,27 @@
 
 // Here are the docs for P5.js: https://p5js.org/reference/
 
-let canvas;
-var widthCanvas=600;
-var heigthCanvas=600;
+let scriptCanvas;
+var widthCanvas;
+var heigthCanvas;
+var Bpressed=true;
 
+  
 // The setup function is called once at the beginning
 function setup() {
+  widthCanvas=windowWidth;
+  heigthCanvas=windowHeight;
   // this is the link from the java script file back to the html file
-  canvas = createCanvas(widthCanvas, heigthCanvas);
-  canvas.parent("canvas-container");
+  //canvas = createCanvas(widthCanvas, heigthCanvas);
+  //let elt = document.getElementById('container');
+  //let div1 = createDiv();
+  //div1.parent(elt); // use element from page
+  //div1.id('canvas-container');
+  //div1.size(windowWidth*0.6, windowHeight);
+  
+  //canvas = createCanvas(windowWidth*0.5, windowHeight*0.5);
+  scriptCanvas = createCanvas(widthCanvas, heigthCanvas);
+  scriptCanvas.parent("canvas-container");
   
   background(255);
   // first I decide on a color I want the lines to be when I start drawing
@@ -26,7 +38,7 @@ function setup() {
 function draw() {
 
   // the line starts drawing with a blue stroke color from the setup.
-  line(mouseX, mouseY, widthCanvas / 2, 780);
+  line(mouseX, mouseY, widthCanvas / 2, heigthCanvas-heigthCanvas/10);
   // the triangle starts drawing 
   fill(255, 255, 255);
   triangle(30, 75, mouseX, mouseY, 86, 75);
@@ -34,8 +46,16 @@ function draw() {
 
 // as soon as the mouse is pressed the stroke color changes to green
 function mousePressed() {
-  stroke(0, 255, 0, mouseY);
-  line(mouseX, mouseY, widthCanvas / 2, 600);
+  if (Bpressed==true){
+    stroke(0, 255, 0, mouseY);
+    Bpressed=false;
+  }
+  else {
+    stroke(0, 0, 255, mouseY);
+    Bpressed=true;
+  }
+    
+  line(mouseX, mouseY, widthCanvas / 2, heigthCanvas-heigthCanvas/10);
 }
 
 function keyTyped() {
